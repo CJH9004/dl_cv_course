a5a3a8d6d0e6c971dae7889bfb2cfb62f8478d28
cjh9004 <2573699004@qq.com>
2020-08-31 23:23:25 +0800

    predict

--- Project4-Self-Drving-Car-Object-Detection/train.py
+++ Project4-Self-Drving-Car-Object-Detection/train.py
@@ -1,20 +1,16 @@
 #%%
 import numpy as np
-from tensorflow.keras.models import Sequential
-import csv
-import cv2
-import glob
-import pickle
-from sklearn.utils import shuffle
-from sklearn.model_selection import train_test_split
-from tensorflow.keras import callbacks, layers, optimizers, losses
-from matplotlib import pyplot
+from tensorflow import keras
+from tensorflow.keras import callbacks, layers, optimizers, losses, Sequential
+import matplotlib.pyplot as plt
 import tensorflow as tf
 import pandas as pd
 import random
 
 SEED = 13
 random.seed(SEED)
+
+AUTOTUNE = tf.data.experimental.AUTOTUNE
 
 def get_model(shape):
     model = Sequential([
@@ -31,12 +27,41 @@ def get_model(shape):
         layers.Dense(1, activation='linear')
     ])
 
-    model.compile(
-        optimizer=optimizers.Adam(learning_rate=1e-2), 
-        loss='mean_squared_error'
-    )
-
     return model
+
+def preprocessing(path, label):
+    img = tf.io.decode_and_crop_jpeg(tf.io.read_file(path), [60, 0, 80, 320])
+    img = tf.image.random_brightness(img, 0.2)
+    if random.randint(0,1) == 0:
+        img = tf.image.flip_left_right(img)
+        label = -label
+    img = tf.image.random_contrast(img, 0.2, 0.5)
+    return img/255.0, label
 
 def load_data():
     d = pd.read_csv('driving_log.csv')[['center', 'steering']]
+    df = d.query('steering != 0').append(d.query('steering == 0').sample(frac=0.05)).sample(frac=1)
+    return tf.data.Dataset.from_tensor_slices((df['center'], df['steering'])).map(preprocessing, num_parallel_calls=AUTOTUNE)
+
+def show(ds):
+    sample = next(iter(ds))
+    print(sample[1])
+    plt.figure()
+    plt.imshow(sample[0])
+    plt.axis('off')
+
+def train():
+    model = get_model((80, 320, 3))
+    model.compile(
+        optimizer=optimizers.Adam(learning_rate=1e-2), 
+        loss='mean_squared_error',
+        metrics=['acc']
+    )
+    ds = load_data()
+    model.fit(ds.prefetch(AUTOTUNE).batch(64), epochs=2)
+    for i in range(10):
+        predict(model, ds)
+
+def predict(model, ds):
+    sample = next(iter(ds))
+    print(model.predict([sample[0]])[0], ', ', sample[1])
